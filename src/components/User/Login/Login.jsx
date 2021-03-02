45638223b598273d6744c97a142b70b81aafc73e
GustavoBazinga <63375206+GustavoBazinga@users.noreply.github.com>
2021-03-02 18:11:12 -0300

    v.3.2.2.3B2
    Comentários de código em src/components/User/Login/Login.jsx

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -9,65 +9,85 @@ import { IoIosArrowForward } from 'react-icons/io';
 import axios from 'axios'
 import './Login.css';
 
+//Função InitialState para iniciar os campos como vazios.
 function initialState() {
   return { login: '', senha: '' };
 }
 
+//Função login executa o login em contato com a API.
 async function login({ login, senha }) {
   var sucess = false
   var localToken
+
+  //Conecta com a API via Axios no metodo POST passando o corpo JSON.
   await axios
         .post('https://sistemaifrj.herokuapp.com/systemlogin',{
           login: login,
           senha: senha
         }
         )
+
+        //Se login Bem Sucedido, sucess e localToken são atribuidos.
         .then(response => {      
           console.log(response)
           console.log(response.data.token)
           sucess = true
           localToken = response.data.token
         })
+
+        //Se login Mal Sucedido, mensagem de erro no console.
+        //PS: Fazer tratamento de erro.
         .catch(error => {
           console.log(error)
         })
+
+        //Se sucess = true, retorna o token de localToken.
         if (sucess){
           return { token: localToken}
         }
+
+        //Se sucess = false, retorna erro.
         else{
           return { error: 'Algo errado'}
         }
 }
 
-const UserLogin = () => {
-  const [values, setValues] = useState(initialState);
-  const [error, setError] = useState(null);
-  const { setToken } = useContext(StoreContext);
-  const history = useHistory();
 
+//Página
+const UserLogin = () => {
+  const [values, setValues] = useState(initialState); //Valores dos campos.
+  const [error, setError] = useState(null); //Erro retornado.
+  const { setToken } = useContext(StoreContext); //Função para estabelecer Token Global com Context.
+  const history = useHistory(); //Paginação.
+
+  //Função onChange atualiza dados dos campos.
   function onChange(event) {
     const { value, name } = event.target;
-
     setValues({
       ...values,
       [name]: value
     });
   }
 
+  //Função onSubmit inicia processo de Login e salva token global.
   async function onSubmit(event) {
     event.preventDefault();
 
+    //Chama login e espera receber token ou error.
     const { token, error } = await login(values);
 
+    //Se receber token, salva token global e redireciona para Home do App.
     if (token) {
-      setToken(token);
-      return history.push('/');
+      setToken(token); //Salva token global.
+      return history.push('/');//Redireciona para Home.
     }
 
+    //Se receber error, salva error e reinicia campos.
     setError(error);
     setValues(initialState);
   }
 
+  //Página HTML
   return (
     <div id="page-login">
       <div className="logo">

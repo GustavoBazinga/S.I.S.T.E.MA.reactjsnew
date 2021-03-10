77edb9f60314dc5d84f99f2a1b0bfb8a081c451c
GustavoBazinga <63375206+GustavoBazinga@users.noreply.github.com>
2021-03-10 00:04:56 -0300

    v4.0.2.10A

--- public/electron.js
+++ public/electron.js
@@ -5,23 +5,27 @@ const path = require("path");
 const isDev = require("electron-is-dev");
 let mainWindow;
 function createWindow() {
-mainWindow = new BrowserWindow({ resizable: false, width: 1280, height: 720 });
-mainWindow.loadURL(
-isDev
-
-? "http://localhost:3000/login"
-: `file://${path.join(__dirname, "../build/index.html")}`
-);
-mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow = new BrowserWindow({
+    resizable: false,
+    width: 1280,
+    height: 720,
+    titleBarStyle: "hidden",
+  });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000/login"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
-if (process.platform !== "darwin") {
-app.quit();
-}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 app.on("activate", () => {
-if (mainWindow === null) {
-createWindow();
-}
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
--- src/pages/Admin/AdminAdd.js
+++ src/pages/Admin/AdminAdd.js
@@ -8,55 +8,62 @@ import "./adminadd.css";
 import axios from "axios";
 
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
-import ReactNotification from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css'
-import { store } from 'react-notifications-component';
-import "animate.css"
-
-
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+import { store } from "react-notifications-component";
+import "animate.css";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
 function initialState() {
   return { login: "", nome: "", email: "", crpsenha: "", crpsenhax: "" };
 }
 
 const AdminAdd = () => {
   const [values, setValues] = useState(initialState);
-  //const {token} = useContext(StoreContext); 
+  //const {token} = useContext(StoreContext);
   //var tokenmineaspas = JSON.stringify(token).replace(/['"]+/g, "");
-  function clearMan(){
-		setValues(initialState);
-	}
+  function clearMan() {
+    setValues(initialState);
+  }
 
   function onSubmit(event) {
     event.preventDefault();
-    if (values.nome === "" || values.login === "" || values.email === "" || values.crpsenha === "" || values.crpsenhax === ""){
+    if (
+      values.nome === "" ||
+      values.login === "" ||
+      values.email === "" ||
+      values.crpsenha === "" ||
+      values.crpsenhax === ""
+    ) {
       store.addNotification({
         title: "Falha!",
-            message: "Preencha todos os campos do formulário!",
-            type: "warning",
-            container: "top-right",
-            insert:"top",
-            animationIn: ["animate__animated", "animate__fadeIn"],
-            animationOut: ["animate__animated", "animate__fadeOut"],
-            dismiss: {
-              duration: 3000,
-              showIcon: true
-            }
-      })
-    }
-    else if (values.crpsenha !== values.crpsenhax) {
+        message: "Preencha todos os campos do formulário!",
+        type: "warning",
+        container: "top-right",
+        insert: "top",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3000,
+          showIcon: true,
+        },
+      });
+    } else if (values.crpsenha !== values.crpsenhax) {
       store.addNotification({
         title: "Falha!",
-            message: "Digite uma senha igual, seu nóia!",
-            type: "warning",
-            container: "top-right",
-            insert:"top",
-            animationIn: ["animate__animated", "animate__fadeIn"],
-            animationOut: ["animate__animated", "animate__fadeOut"],
-            dismiss: {
-              duration: 3000,
-              showIcon: true
-            }
-      })
+        message: "Digite uma senha igual, seu nóia!",
+        type: "warning",
+        container: "top-right",
+        insert: "top",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 3000,
+          showIcon: true,
+        },
+      });
     } else {
       axios
         .post("https://sistemaifrj.herokuapp.com/admins/", values)
@@ -67,15 +74,15 @@ const AdminAdd = () => {
             message: "Adminstrador Cadastrado com Sucesso!",
             type: "success",
             container: "top-right",
-            insert:"top",
+            insert: "top",
             animationIn: ["animate__animated", "animate__fadeIn"],
             animationOut: ["animate__animated", "animate__fadeOut"],
             dismiss: {
               duration: 3000,
-              showIcon: true
-            }
-          })
-          clearMan()
+              showIcon: true,
+            },
+          });
+          clearMan();
         })
         .catch((error) => {
           store.addNotification({
@@ -83,15 +90,15 @@ const AdminAdd = () => {
             message: "ERICK, FAÇA UM RETORNO DE ERRO MELHOR!",
             type: "danger",
             container: "top-right",
-            insert:"top",
+            insert: "top",
             animationIn: ["animate__animated", "animate__fadeIn"],
             animationOut: ["animate__animated", "animate__fadeOut"],
             dismiss: {
               duration: 3000,
-              showIcon: true
-            }
-          })
-          clearMan()
+              showIcon: true,
+            },
+          });
+          clearMan();
         });
     }
   }
@@ -104,77 +111,140 @@ const AdminAdd = () => {
     });
   }
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      marginTop: "10ch",
+
+      "& > *": {
+        width: "80ch",
+        marginLeft: "35ch",
+        marginTop: "2ch",
+      },
+
+      "& div.buttonGeral": {
+        paddingLeft: "54.45ch",
+        //marginTop: "-5ch",
+      },
+      "& button.btnLimpar": {
+        width: "12ch",
+        fontSize: "2.5ch",
+      },
+      "& button.btnSalvar": {
+        width: "12ch",
+        fontSize: "2.5ch",
+      },
+      "& label.Mui-focused": {
+        color: "gray",
+      },
+
+      "& .MuiOutlinedInput-root": {
+        "&.Mui-focused fieldset": {
+          borderColor: "gray",
+        },
+      },
+    },
+
+    email: {
+      width: "38ch",
+    },
+
+    buttonSalvar: {
+      "&:hover": {
+        backgroundColor: "green",
+      },
+      backgroundColor: "gray",
+      width: "18ch",
+      marginLeft: "1ch",
+    },
+    buttonLimpar: {
+      backgroundColor: "white",
+    },
+
+    login: {
+      width: "38ch",
+      marginLeft: "4ch",
+    },
+    senha: {
+      width: "38ch",
+    },
+    senhaConfirmar: {
+      width: "38ch",
+      marginLeft: "4ch",
+    },
+  }));
+
+  const classes = useStyles();
   return (
     <div>
-      <ReactNotification/>
-      <Sidebar />
+      <ReactNotification />
+      <div className="sidebarAddAdm">
+        <Sidebar />
+      </div>
       <div className="titleAdmAdd">
         <h1>Cadastrar Adminstrador</h1>
       </div>
-      <form onSubmit={onSubmit} className="form_addAdmin" name="FormAdmAdd">
-        <div className="inputAdmNome">
-          <input
-            type="text"
-            name="nome"
-            value={values.nome}
-            onChange={OnChange}
-            required
-          />
-          <div className="underlineAdmNome"></div>
-          <label className="labelAdmNome">Nome</label>
+      <form onSubmit={onSubmit} className={classes.root} name="FormAdmAdd">
+        <TextField
+          id="outlined-basic"
+          name= "nome"
+          value = {values.nome}
+          onChange={OnChange}
+          label="Nome Completo"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="E-mail"
+          name= "email"
+          value = {values.email}
+          onChange={OnChange}
+          className={classes.email}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Login"
+          name= "login"
+          value = {values.login}
+          onChange={OnChange}
+          className={classes.login}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          type= "password"
+          label="Senha"
+          name= "crpsenha"
+          value = {values.crpsenha}
+          onChange={OnChange}
+          className={classes.senha}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          type= "password"
+          name= "crpsenhax"
+          value = {values.crpsenhax}
+          onChange={OnChange}
+          label="Confirmar Senha"
+          className={classes.senhaConfirmar}
+          variant="outlined"
+        />
+        <div className="buttonGeral">
+          {/* <button type="button" className="btnLimpar">Limpar</button> */}
+          {/* <button type="button" className="btnSalvar">Salvar</button> */}
+          <Button variant="contained" onClick={clearMan} className={classes.buttonLimpar}>
+            Limpar
+          </Button>
+          <Button
+          type= "submit"
+            variant="contained"
+            color="primary"
+            className={classes.buttonSalvar}
+          >
+            Salvar
+          </Button>
         </div>
-        <div className="inputAdmEmail">
-          <input
-            type="text"
-            name="email"
-            required
-            value={values.email}
-            onChange={OnChange}
-          />
-          <div className="underlineAdmEmail"></div>
-          <label className="labelAdmEmail">Email</label>
-        </div>
-        <div className="inputAdmLogin">
-          <input
-            type="text"
-            name="login"
-            required
-            value={values.login}
-            onChange={OnChange}
-          />
-          <div className="underlineAdmLogin"></div>
-          <label className="labelAdmLogin">Login </label>
-        </div>
-
-        <div className="inputAdmSenha">
-          <input
-            type="password"
-            name="crpsenha"
-            required
-            value={values.crpsenha}
-            onChange={OnChange}
-          />
-          <div className="underlineAdmSenha"></div>
-          <label className="labelAdmSenha">Senha </label>
-        </div>
-        <div className="inputAdmSenhaConfirmar">
-          <input
-            type="password"
-            name="crpsenhax"
-            required
-            value={values.crpsenhax}
-            onChange={OnChange}
-          />
-          <div className="underlineAdmSenhaConfirmar"></div>
-          <label className="labelAdmSenhaConfirmar">Confirmar Senha </label>
-        </div>
-
-        <button type="submit" className="btnAddAdm">
-          Salvar
-        </button>
-        <button type="button" className="btnAddAdmLimpar" onClick={clearMan}>
-          Limpar
-        </button>
       </form>
     </div>
   );
